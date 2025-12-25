3cae80e3f502990779507313a9082914fbba4736
Duncan <fjf@hotmail.nl>
2025-12-25 12:23:12 +0100

    Add electron overlay for the UI

---
+++ app/electron/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        fullscreen: true,
+        transparent: true,
+        frame: false,
+        alwaysOnTop: true,
+        resizable: false,
+        hasShadow: false,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
